263101b34588fd568a8571ce419b6f15c5698bd3
Chris Cooper <cjcooper252@gmail.com>
2017-02-01 16:57:29 +0000

    creates user model

---
+++ models/user.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+
+var userSchema = new Schema({
+  name: String,
+  username: String,
+  email: String,
+  location: String,
+  genres: String,
+  instruments: String,
+  bio: String
+})
+
+var User = mongoose.model('User', userSchema)
+
+module.exports = User
