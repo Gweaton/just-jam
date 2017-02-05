e1a06103f4b68f8f89145e266d8cbd51562f7b9d
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-02-05 18:57:31 +0000

    Added array to hold messages in that chat

--- models/chat.js
+++ models/chat.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var chatSchema = new Schema({
-  participants: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+  participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  messages: [{ type: Schema.Types.ObjectId, ref: 'Message'}]
 })
 
 var Chat = mongoose.model('Chat', chatSchema)
