0539a621c90da58176b94fb18adb9b76ea7f977e
Jenna Ramdenee <jenna_ramdenee@hotmail.co.uk>
2017-02-06 19:25:36 +0000

    Added sender and recipient fields to model

--- models/chat.js
+++ models/chat.js
@@ -3,6 +3,8 @@ var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var chatSchema = new Schema({
+  sender: String,
+  recipient: String,
   participants: [{ type: Schema.Types.ObjectId, ref: 'User' }],
   messages: [{ type: Schema.Types.ObjectId, ref: 'Message'}]
 })
