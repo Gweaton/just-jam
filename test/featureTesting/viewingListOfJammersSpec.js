0b7eeea82e067e35f8435ac477ee015d2cc1abcc
Chris Cooper <cjcooper252@gmail.com>
2017-02-07 11:10:02 +0000

    tests viewing list of jammers

--- test/featureTesting/viewingListOfJammersSpec.js
+++ test/featureTesting/viewingListOfJammersSpec.js
@@ -5,21 +5,27 @@ var Browser = require('zombie');
 
 var assert = require('assert');
 var User = require('../../models/user');
+var Jammer = require('../../models/jammer');
+var mongoose = require('mongoose')
 var helper = require('./testHelper');
 
 
 Browser.localhost('localhost', 3001);
 
 describe('Viewing other jammers', function() {
-  User.collection.drop();
 
   const browser = new Browser();
 
   before(function(done) {
-    browser.visit('/jammers/', done);
+    mongoose.model('Jammer').remove({}, function(err) {
+      if (err) throw err;
+    });
+    mongoose.model('Jammer').create({name: "Zombie"});
+    browser.visit('/jammers', done);
   });
 
   it('should show all the signed up jammers', function() {
     browser.assert.text('h1', 'Jammers');
+    browser.assert.text('h2', 'Zombie');
   });
 });
