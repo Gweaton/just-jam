9bd99b30dd7526fbeea6d658891261ae101baa77
pflarcology <rhrg500@gmail.com>
2017-02-09 09:57:30 +0000

    removes commented out old routes

--- app.js
+++ app.js
@@ -70,25 +70,6 @@ app.use('/jammers', jammers);
 
 require('./config/passport')(passport);
 
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
 //socket authentication
 io.use(passportSocketIo.authorize({
   cookieParser: cookieParser,
